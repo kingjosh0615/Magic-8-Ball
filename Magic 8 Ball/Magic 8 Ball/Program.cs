9142013691f7d855d3c3a50364e4e3756fed2910
kingjosh0615 <54904560+kingjosh0615@users.noreply.github.com>
2020-09-15 18:35:46 -0400

    Part 2 Final

--- Magic 8 Ball/Magic 8 Ball/Program.cs
+++ Magic 8 Ball/Magic 8 Ball/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace Magic_8_Ball
 {
@@ -13,20 +19,24 @@ namespace Magic_8_Ball
             while (x == 1)
             {
                 Random rand1 = new Random();
+                int numberOfSecondsToSleep = rand1.Next(1, 6);
                 Console.ForegroundColor = (ConsoleColor)rand1.Next(15);
                 Console.WriteLine("Enter your question");
                 string question = Console.ReadLine();
+                Console.WriteLine("Hmmm... let me think...");
+                Thread.Sleep(numberOfSecondsToSleep * 1000);
+
                 if (question == "Are you real?")
-                { 
+                {
+                    OpenBrowser("https://pointerpointer.com/");
                 }
                 else if (question == "I hate you.")
-                { 
+                {
                     while (y == 1)
                     {
                         Console.WriteLine("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
-                        
-                        using (var proc = Process.Start(@"cmd.exe ", @"/c C:\Users\WILES_JOSHUA\Pictures\Pravin_Raut-min.jpg")) { }
-                       
+
+                        using var proc = Process.Start(@"cmd.exe ", @"/c C:\Users\WILES_JOSHUA\Pictures\Pravin_Raut-min.jpg");
                     }
                 }
                 else if (question == "Red Robin")
@@ -39,31 +49,32 @@ namespace Magic_8_Ball
                 }
                 else
                 {
-                    
+
+
+                    string[] magicAnswers = {
+                "YES!",
+                "NO!",
+                "I don't understand the question.",
+                "Probably not.",
+                "Possibly?",
+                "Totally.",
+                "Not possible.",
+                "Try again.",
+                "That's kinda a dumb question.",
+                "( ͡° ͜ʖ ͡°)",
+                "Nope.",
+                "Nani?!",
+                "Yes.",
+                "Go eat a sandwich.",
+                "You've had enough computer time.",
+                "The council will decide your fate.",
+                "I'm not entirely sure.",
+                };
+
+                    double randomAnswer = rand1.Next(1, 17);
+
+                    Console.WriteLine("The great magic 8 ball says: " + magicAnswers[Convert.ToInt32(Math.Floor(randomAnswer))]);
                 }
-                string[] magicAnswers = {
-    "YES!",
-    "NO!",
-    "I don't understand the question.",
-    "Probably not.",
-    "Possibly?",
-    "Totally.",
-    "Not possible.",
-    "Try again.",
-    "That's kinda a dumb question.",
-    "( ͡° ͜ʖ ͡°)",
-    "Nope.",
-    "Nani?!",
-    "Yes.",
-    "Go eat a sandwich.",
-    "You've had enough computer time.",
-    "The council will decide your fate.",
-    "I'm not entirely sure.",
-            };
-                
-                double randomAnswer = rand1.Next(1, 17);
-                
-                Console.WriteLine("The great magic 8 ball says: " + magicAnswers[Convert.ToInt32(Math.Floor(randomAnswer))]);
                 Console.WriteLine("Do you wish to ask another question?  'Y' for Yes, 'N' for No.");
                 string playAgain = Console.ReadLine().ToLower();
                 switch (playAgain)
@@ -77,6 +88,36 @@ namespace Magic_8_Ball
                         break;
                 }
             }
+
+            static void OpenBrowser(string url)
+            {
+                try
+                {
+                    Process.Start(url);
+                }
+                catch
+                {
+                    // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        url = url.Replace("&", "^&");
+                        Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                    }
+                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    {
+                        Process.Start("xdg-open", url);
+                    }
+                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                    {
+                        Process.Start("open", url);
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+
         }
     }
 }
