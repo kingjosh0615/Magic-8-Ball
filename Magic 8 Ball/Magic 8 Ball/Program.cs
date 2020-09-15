cdce0eaa14422b684b03b68c9c8952a1693681b4
Wiles_Joshua <Wiles_Joshua@edu.mcctc.net>
2020-09-15 13:04:31 -0400

    Part 2 (Be careful)

--- Magic 8 Ball/Magic 8 Ball/Program.cs
+++ Magic 8 Ball/Magic 8 Ball/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Magic_8_Ball
 {
@@ -7,10 +8,43 @@ namespace Magic_8_Ball
         static void Main(string[] args)
         {
             int x = 1;
+            int y = 1;
+            //string target = "http://www.mommytimes.net";
             while (x == 1)
             {
+                Random rand1 = new Random();
+                Console.ForegroundColor = (ConsoleColor)rand1.Next(15);
                 Console.WriteLine("Enter your question");
                 string question = Console.ReadLine();
+                if (question == "Are you real?")
+                { 
+                }
+                else if (question == "I hate you.")
+                { 
+                    while (y == 1)
+                    {
+                        Console.WriteLine("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+                        //Process.Start("C:\Users\WILES_JOSHUA\Pictures\candont.jpg");
+                        using (var proc = Process.Start(@"cmd.exe ", @"/c C:\Users\WILES_JOSHUA\Pictures\Pravin_Raut-min.jpg")) { }
+                        //Process photoViewer = new Process();
+                        //photoViewer.StartInfo.FileName = @"The photo viewer file path";
+                        //photoViewer.StartInfo.Arguments = @"Your image file path";
+                        //photoViewer.Start();
+                        //C: \Users\WILES_JOSHUA\Desktop
+                    }
+                }
+                else if (question == "Red Robin")
+                {
+                    Console.WriteLine("Yum");
+                }
+                else if (question == "Smitty is cool.")
+                {
+                    Console.WriteLine("No hes not.");
+                }
+                else
+                {
+                    
+                }
                 string[] magicAnswers = {
     "YES!",
     "NO!",
@@ -27,11 +61,12 @@ namespace Magic_8_Ball
     "Yes.",
     "Go eat a sandwich.",
     "You've had enough computer time.",
-    "The counsel will decide your fate.",
+    "The council will decide your fate.",
     "I'm not entirely sure.",
             };
-                Random rand1 = new Random();
+                
                 double randomAnswer = rand1.Next(1, 17);
+                
                 Console.WriteLine("The great magic 8 ball says: " + magicAnswers[Convert.ToInt32(Math.Floor(randomAnswer))]);
                 Console.WriteLine("Do you wish to ask another question?  'Y' for Yes, 'N' for No.");
                 string playAgain = Console.ReadLine().ToLower();
